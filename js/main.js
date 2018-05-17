5e14f284654d461d661213a0999a6616414a94fa
zoombya <progolab@gmail.com>
2018-05-16 18:17:22 -0700

    save image on p press

--- js/main.js
+++ js/main.js
@@ -213,6 +213,19 @@ function onWindowResize() {
 
 }
 
+//Save image on "p" press 
+document.addEventListener("keypress", event =>{
+    if(event.keyCode === 112){
+        renderer.domElement.toBlob(function(blob){
+        var a = document.createElement('a');
+        var url = URL.createObjectURL(blob);
+        a.href = url;
+        a.download = 'canvas.png';
+        a.click();
+        }, 'image/png', 1.0);
+    }
+});
+
 
 
 
