53caf43d25cd3da34d589a7feba5b8cd8231cd1b
ErikPoppleton <epopplet@asu.edu>
2018-05-21 20:59:14 -0700

    Nucleotides now incorporated into new structure.  Still need to change display functions

--- js/main.ts
+++ js/main.ts
@@ -10,18 +10,21 @@ class Nucleotide {
     neighbor3:number;
     neighbor5:number;
     pair:number; //this is going to be optional
-    type:number; // 0:A 1:G 2:C 3:T
-    backbone_obj:THREE.Object3D[]
-    nuc_obj:THREE.Object3D[] //hopefully these can know their position/orientation
+    type:number|string; // 0:A 1:G 2:C 3:T/U
+    parent_strand:number;
+    backbone_obj:THREE.Mesh;
+    nuc_obj:THREE.Mesh; //hopefully these can know their position/orientation
+    bb_nuc_con:THREE.Mesh;
+    bb_bb_con:THREE.Mesh;
 
 
-    constructor(global_id:number, local_id:number, pos:number[], neighbor3:number, neighbor5:number, type:number){
+    constructor(global_id:number, local_id:number, neighbor3:number, neighbor5:number, type:number|string, parent_strand:number){
         this.global_id = global_id;
         this.local_id = local_id;
-        this.pos = pos;
         this.neighbor3 = neighbor3;
         this.neighbor5 = neighbor5;
         this.type = type;
+        this.parent_strand = parent_strand;
 
     };
 };
@@ -31,7 +34,7 @@ class Nucleotide {
 class Strand {
 
     strand_id:number;
-    nucleotides:Nucleotide[];
+    nucleotides:Nucleotide[] = [];
 
 
     constructor(id) {
@@ -51,7 +54,7 @@ class Strand {
 class System {
 
     system_id:number;
-    strands:Strand[];
+    strands:Strand[] = [];
 
     constructor(id) {
         this.system_id = id;
@@ -59,7 +62,6 @@ class System {
 
     add_strand(strand:Strand){
         this.strands.push(strand);
-        strand.strand_id = this.strands.indexOf(strand);
     };
 
     //remove_strand(){};
@@ -113,6 +115,12 @@ var selected_bases = {};
     }
 });*/
 
+//initialize the space
+let systems: System[] = [];
+let sys_count:number = 0;
+let strand_count:number = 0;
+let nuc_count:number = 0;
+
 // define the drag and drop behavior of the scene 
 var target = renderer.domElement;
 target.addEventListener("dragover", function(event) {
@@ -123,6 +131,9 @@ target.addEventListener("drop", function(event) {
 
     // cancel default actions
     event.preventDefault();
+
+    //make system to store the dropped files in
+    var system = new System(sys_count);
 
     var i = 0,
         files = event.dataTransfer.files,
@@ -155,24 +166,43 @@ target.addEventListener("drop", function(event) {
     //read topology file
     let top_reader = new FileReader();
     top_reader.onload = ()=> {
-        // parse file into lines 
+        // make first strand
+        let current_strand = new Strand(0);
+        let nuc_local_id: number = 0;
+        let last_strand: number = 1; //strands are 1-indexed in oxDNA .top files
+        // parse file into lines
         var lines = top_reader.result.split(/[\r\n]+/g);
         lines = lines.slice(1); // discard the header  
         lines.forEach(
             (line, i) => {
-                let l = line.split(" "); 
-                let id = parseInt(l[0]); // get the strand id 
+                let l = line.split(" "); //split the file and read each column
+                let id = parseInt(l[0]); // get the strand id
+                if (id != last_strand){
+                    current_strand = new Strand(id);
+                    nuc_local_id = 0;
+                };
                 let base = l[1]; // get base id
-                //if we meet a U we have an RNA 
+                let neighbor3 = l[2];
+                let neighbor5 = l[3];
+
+                //if we meet a U we have an RNA (its dumb, but its all we got)
                 if(base === "U"){
                     RNA_MODE = true;
                 }
+
+                let nuc = new Nucleotide(nuc_count, nuc_local_id, neighbor3, neighbor5, base, id);
+                current_strand.add_nucleotide(nuc);
+                nuc_count += 1;
+                if (neighbor5 == -1){
+                    system.add_strand(current_strand);
+                };
                 // create a lookup for
                 // coloring base according to base id
                 base_to_material[i] = nucleoside_materials[base_to_num[base]];
                 // coloring bases according to strand id 
                 strand_to_material[i] = backbone_materials[Math.floor(id % backbone_materials.length )];
             });
+        systems.push(system);
     };
     top_reader.readAsText(top_file);
 
@@ -182,8 +212,11 @@ target.addEventListener("drop", function(event) {
         z_bb_last;
     var last_strand;
 
+
     let dat_reader = new FileReader();
-    dat_reader.onload = ()=>{
+    dat_reader.onload = ()=>{ //this is a function, it can't see the system
+        var current_nuc_local_id = 0;
+        var current_strand = systems[sys_count].strands[0];
         // parse file into lines 
         var lines = dat_reader.result.split(/[\r\n]+/g);
         
@@ -202,9 +235,12 @@ target.addEventListener("drop", function(event) {
         
         // add the bases to the scene
         lines.forEach((line, i) => {
+            if (line == ""){return};
+            console.log(current_nuc_local_id)
+            var current_nucleotide = current_strand.nucleotides[current_nuc_local_id];
+            //get nucleotide information
             // consume a new line 
             let l:string = line.split(" ");
-            
             // shift coordinates such that the 1st base of the  
             // 1st strand is @ origin 
             let x = parseFloat(l[0])- fx, 
@@ -220,6 +256,7 @@ target.addEventListener("drop", function(event) {
             x = x - dx;
             y = y - dy;
             z = z - dz;
+            current_nucleotide.pos = [x, y, z];
 
             // extract axis vector a1 (backbone vector) and a3 (stacking vector) 
             let x_a1 = parseFloat(l[3]),
@@ -299,6 +336,9 @@ target.addEventListener("drop", function(event) {
             backbone.position.set(x_bb, y_bb, z_bb); 
             nucleoside.position.set(x_ns, y_ns, z_ns);
             con.position.set(x_con, y_con, z_con);
+            current_nucleotide.backbone_obj = backbone;
+            current_nucleotide.nuc_obj = nucleoside;
+            current_nucleotide.bb_nuc_con = con;
 
             //last, add the sugar-phosphate bond since its not done for the first nucleotide in each strand
             if(x_bb_last != undefined && strand_to_material[i] == last_material){
@@ -320,21 +360,49 @@ target.addEventListener("drop", function(event) {
                 connectors.push(sp);
                 scene.add(sp);
                 sp.position.set(x_sp, y_sp, z_sp);
-            }
+                current_nucleotide.bb_bb_con = sp;
+            };
 
             //update last backbone position and last strand
             x_bb_last = x_bb;
             y_bb_last = y_bb;
             z_bb_last = z_bb;
             last_material= strand_to_material[i];
+            if (current_nucleotide.neighbor5 == -1){
+                current_strand = system.strands[current_strand.strand_id + 1];
+                current_nuc_local_id = 0;
+            }
+            else{
+                current_nuc_local_id += 1;
+            };
 
         });
+
+        // reposition center of mass of the system to 0,0,0
+        let cms = new THREE.Vector3(0,0,0);
+        let n_nucleosides = nucleosides.length; 
+        let i = 0;
+        for(; i < n_nucleosides; i++){
+            cms.add(nucleosides[i].position);
+        }
+        let mul = 1.0 / n_nucleosides;
+        cms.multiplyScalar(mul);
+        for(i = 0; i < n_nucleosides; i++){
+            nucleosides[i].position.sub(cms);
+            backbones[i].position.sub(cms);
+        }
+        connectors.forEach(cns =>{
+            cns.position =  cns.position.sub(cms);
+        });
+        
         // update the scene
         render();
         
     };
     // execute the read operation 
     dat_reader.readAsText(dat_file);
+    systems.push(system);
+    sys_count += 1;
 
 }, false);
 
