48138b97bba6d9fa07bcdaedd350fb97fdbe3dee
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2021-01-31 15:50:53 +0000

    Make live relax use new conf update code
    This makes it possible to design and simulate without export
    and import in betwen

--- dist/ox_serve/live_relax.js
+++ dist/ox_serve/live_relax.js
@@ -67,28 +67,7 @@ class OXServeSocket extends WebSocket {
                 console.log(message["console_log"]);
             }
             if ("dat_file" in message) {
-                let lines = message["dat_file"].split("\n");
-                lines = lines.slice(3); // discard the header
-                let system = systems[systems.length - 1];
-                let numNuc = system.systemLength(); //gets # of nuc in system
-                let currentNucleotide, l;
-                for (let lineNum = 0; lineNum < numNuc; lineNum++) {
-                    currentNucleotide = elements.get(systems[systems.length - 1].globalStartId + lineNum);
-                    // consume a new line
-                    l = lines[lineNum].split(" ");
-                    currentNucleotide.calculateNewConfigPositions(l);
-                }
-                system.backbone.geometry["attributes"].instanceOffset.needsUpdate = true;
-                system.nucleoside.geometry["attributes"].instanceOffset.needsUpdate = true;
-                system.nucleoside.geometry["attributes"].instanceRotation.needsUpdate = true;
-                system.connector.geometry["attributes"].instanceOffset.needsUpdate = true;
-                system.connector.geometry["attributes"].instanceRotation.needsUpdate = true;
-                system.bbconnector.geometry["attributes"].instanceOffset.needsUpdate = true;
-                system.bbconnector.geometry["attributes"].instanceRotation.needsUpdate = true;
-                system.bbconnector.geometry["attributes"].instanceScale.needsUpdate = true;
-                system.dummyBackbone.geometry["attributes"].instanceOffset.needsUpdate = true;
-                centerAndPBC();
-                render();
+                updateConfFromFile(message["dat_file"]);
             }
         };
         this.onopen = (resonse) => {
