2500e1b6628ea1d310fb17543c4b2fb5705bcfad
Aatmik Mallya <aatmik54@gmail.com>
2021-04-19 16:27:23 -0700

    add nanobase IP to whitelist

--- dist/file_handling/file_reading.js
+++ dist/file_handling/file_reading.js
@@ -719,6 +719,7 @@ function addSystemToScene(system) {
 const whitelist = new Set([
     "http://localhost:9000",
     "http://localhost:8000",
+    "http://206.207.50.88",
     "http://nanobase.org",
     "https://oxdna.org",
     "https://sulcgroup.github.io" //for some reason the iframe on Chrome thinks this is the message source
