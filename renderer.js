53277da6bfd13f0865d91d40f29350547cd7ed7d
zoombya <progolab@gmail.com>
2021-08-19 15:15:32 -0700

    adding repo to package

--- renderer.js
+++ renderer.js
@@ -1,5 +1,11 @@
 // section responsible for electron interaction
 if(typeof require !== undefined) {
+    //we are in electron
+    //ccCapture is not working ;(
+    let btn = document.getElementById("videoCreateButton");
+    btn.disabled = true; 
+    btn.style.visibility="hidden";
+
     const remote = require('electron').remote;
     let arguments = remote.getGlobal('sharedObject').argv;
     
