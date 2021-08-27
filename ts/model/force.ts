022c377af17214e4554815c4ade5305c2aaff9a7
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2021-08-27 15:17:46 +0100

    Fix typo in dialogue

--- ts/model/force.ts
+++ ts/model/force.ts
@@ -206,7 +206,7 @@ function makeTrapsFromPairs() {
         }
     });
     if (nopairs) {
-        ask("No basepair info found", "Do you run an automatic basepair search?",
+        ask("No basepair info found", "Do you want to run an automatic basepair search?",
         ()=>{view.longCalculation(findBasepairs, view.basepairMessage,()=>{
             makeTrapsFromPairs(); listForces(); // recall this as we now have pairs
         })})
