238491ef9d81c183bebb44452e8f3fb6b0845056
sme777 <samson.petrosyan8@gmail.com>
2022-05-04 17:22:07 -0700

    [TADO-9] add OrigamiStrand class

--- dist/model/origamiStrand.js
+++ dist/model/origamiStrand.js
@@ -1,5 +1,7 @@
 class OrigamiStrand extends Strand {
-    constructor(id, system) {
+    constructor(id, system, velocity=10, direction=THREE.Vector3(0, 0, 1)) {
         super(id, system);
+        this.velocity = velocity;
+        this.direction = direction;
     }
 }
