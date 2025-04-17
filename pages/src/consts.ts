bc77503d2180a73c0fd1ef1abdf51d13af7ea3a1
Navraj Sikand <nonunavraj@gmail.com>
2025-04-17 10:42:34 -0700

    api change

--- pages/src/consts.ts
+++ pages/src/consts.ts
@@ -1,5 +1,5 @@
-export const apiRoot =
-  process.env.NODE_ENV === "production"
-    ? "https://api.nanobase.org/api/v1"
-    : "http://localhost:3002/api/v1";
-//export const apiRoot = "http://localhost:3002/api/v1";
+//export const apiRoot =
+//  process.env.NODE_ENV === "production"
+//    ? "https://api.nanobase.org/api/v1"
+//    : "http://localhost:3002/api/v1";
+export const apiRoot = "http://localhost:3002/api/v1";
