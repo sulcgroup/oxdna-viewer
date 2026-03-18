b3f8971bc0d11d615cec59d519e96f8d0c075a51
Subhajit Roy <me.subhajitroy1999@gmail.com>
2026-03-17 17:15:20 -0700

    Improve par file parsing

--- ts/file_handling/aux_readers.ts
+++ ts/file_handling/aux_readers.ts
@@ -267,8 +267,10 @@ function readSelectFile(file:File) { // TODO: needs further checking and integra
 
 //reads in an anm parameter file and associates it with the last loaded system.
 function parsePar(lines, system) {
-
-    lines = lines.split(/[\n]+/g);
+    lines = lines
+        .split(/\r?\n/g)
+        .map(line => line.trim())
+        .filter(line => line.length > 0 && !line.startsWith('#'));
     //remove the header
     lines = lines.slice(1)
 
